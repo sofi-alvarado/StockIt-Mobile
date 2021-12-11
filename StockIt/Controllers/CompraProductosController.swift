c53f34fd6606a78683132d3ed6f94e943314aa30
Hugo Rodriguez <hugsrodriguez8@gmail.com>
2021-12-10 17:57:26 -0800

    Agregacion de metodos de insercion

--- StockIt/Controllers/CompraProductosController.swift
+++ StockIt/Controllers/CompraProductosController.swift
@@ -1,12 +1,36 @@
 
 import UIKit
 
-
+extension String {
+    func isValidDouble(maxDecimalPlaces: Int) -> Bool {
+        
+        // Usamos NumberFormatter para verificar si podemos convertir la cadena en numero
+        // y ubicar el punto decimal.
+        let formatter = NumberFormatter()
+        formatter.allowsFloats = true
+        let decimalSeparator = formatter.decimalSeparator ?? "."  // Obtenemos la ubicacion especifica del punto decimal. Si no hay uno, asumimos "." como valor.
+        
+        // Verificamos si podemos crear un numero valido
+        if formatter.number(from: self) != nil {
+            
+            // Separamos nuestra cadena en el punto decimal
+            let split = self.components(separatedBy: decimalSeparator)
+            
+            let digits = split.count == 2 ? split.last ?? "" : ""
+            
+            
+            return digits.count <= maxDecimalPlaces
+        }
+        
+        return false
+    }
+}
 
 class CompraProductos: UIViewController, UITextFieldDelegate {
     
     //Variables
     var idProductoParametro:Int = 0//Variable a pasar desde pantalla Productos
+    var idProveedorParametro:Int = 0//Variable a pasar desde pantalla Productos
     var nomProveedor:String = ""
     var nomCategoria:String = ""
     var nomProducto:String = ""
@@ -40,7 +64,11 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
         lblDetalles.text = detalles
         
         //Asignamos delegates
+        txtPrecioLote.delegate = self
+        txtPrecioUnitario.delegate = self
         txtPorcentajeGanancia.delegate = self
+        txtGanancia.delegate = self
+        txtPrecioVenta.delegate = self
         
     }
 
@@ -60,6 +88,14 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
                 return false
             }
             
+        } else if textField == txtPrecioLote {
+            
+            let currentText = textField.text ?? ""
+            let replacementText = (currentText as NSString).replacingCharacters(in: range, with: string)
+            
+            // Use our string extensions to check if the string is a valid double and
+            // only has the specified amount of decimal places.
+            return replacementText.isValidDouble(maxDecimalPlaces: 2)
         } else {
             return false
         }
@@ -68,8 +104,18 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
         if textField == txtPorcentajeGanancia {
-            let porcentajeGananciaS = txtPorcentajeGanancia.text != nil ? txtPorcentajeGanancia.text : "0"
-            porcentajeGanancia = (porcentajeGananciaS! as NSString).doubleValue / 100
+            
+            let valorTextField = txtPorcentajeGanancia.text != nil ? txtPorcentajeGanancia.text : "0"
+            porcentajeGanancia = (valorTextField! as NSString).doubleValue / 100
+            
+            calcGananciaYPrecioVenta()
+            
+        } else if textField == txtPrecioLote {
+            
+            let valorTextField = txtPrecioLote.text != nil ? txtPrecioLote.text : "0"
+            precioLote = (valorTextField! as NSString).doubleValue
+            
+            calcGananciaYPrecioVenta()
         }
     }
     //MARK: Control de TextField onChange - Fin
@@ -79,12 +125,22 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
     @IBAction func stepperCantidad(_ sender: UIStepper) {
         cantidad = Int(sender.value)
         lblCantidad.text = String(cantidad)
+        
+        calcGananciaYPrecioVenta()
     }
     
     //MARK: Agregar/Disminuir cantidad - Fin
     
     //MARK: Metodos de calculos - Inicio
     func calcGananciaYPrecioVenta(){
+        precioUnitario = (precioLote / Double(cantidad))
+        txtPrecioUnitario.text = String(format: "%.2f", precioUnitario)
+        
+        ganancia = (precioUnitario * porcentajeGanancia)
+        txtGanancia.text = String(format: "%.2f", ganancia)
+        
+        precioVenta = (precioUnitario + ganancia)
+        txtPrecioVenta.text = String(format: "%.2f", precioVenta)
         
     }
     //MARK: Metodos de calculos - Fin
@@ -117,20 +173,108 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
             self.present(alert, animated: true)
             
         } else {
-            print("Paso las validaciones")
             
+            var message:String = ""//Variable para mostrar alert
             
-            /*insertarProductos(idCategoria: 0, idUsuario: 1, nombreProducto: "Producto", precio: 5.0, existencia: 5, detalles: "Detalles"){
+            insertarEncabezadoCompra(idProveedor: idProveedorParametro, monto: precioLote){
                 (r) in
-                print("Resultado compra: \(String(r))")
-            }*/
+                
+                if r > 0 {
+                    //Insertar detalle
+                    self.insertarDetalleCompraPoductos(idEncabezado: r, idProducto: self.idProductoParametro, cantidad: self.cantidad, precioLote: self.precioLote, precioUnitario: self.precioUnitario, precioUnitarioReal: self.precioVenta, porcentajeGanancia: self.porcentajeGanancia){
+                        (r) in
+                        
+                        print("Resultado compra: \(String(r))")
+                    }
+                    
+                } else if r == -1 {
+                    
+                    message = "No puedes agregar un nuevo lote, hay uno en existencia."
+                    
+                } else {
+                    
+                    message = "Hubo un error. Intente mas tarde."
+                    
+                }
+                
+                let alert = UIAlertController(title: "Alerta", message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                
+                self.present(alert, animated: true)
+            }
+            
             //Insertar compra
         }
         
     }
     
-    //Metodo para comprar productos
-    func insertarProductos(idCategoria:Int, idUsuario:Int, nombreProducto:String, precio:Double, existencia:Int, detalles:String, completion:@escaping(_ r:Int) -> ()){
+    //Metodo para insertar encabezado compra
+    func insertarEncabezadoCompra(idProveedor:Int, monto:Double, completion:@escaping(_ r:Int) -> ()){
+        
+        //192.168.1.8
+        let url=URL(string: "http://192.168.1.8/WebService/WebServiceSI.asmx/insertarEncabezadoCompraJSON")!
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        let parameters: [String: Any] = [
+            "idProveedor": idProveedor,
+            "monto" : monto,
+        ]
+        
+        request.httpBody = parameters.percentEncoded()
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            guard let data = data else { //si existe un error se termina la funcion
+                print("solicitud fallida \(String(describing: error))") //manejamos el error
+                
+                completion(-2)
+                return
+            }
+            
+            do { //creamos nuestro objeto json
+                
+                print("recibimos repuesta")
+                
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [[String:Any]]
+                {
+                    
+                    DispatchQueue.main.async {
+                        
+                        print("Datos completos: \(json)")
+                        
+                        let encabezadoCompra:MEncabezadoCompraProductos = MEncabezadoCompraProductos()
+                        
+                        for encabezadoCompraJSON in json{
+                            
+                            encabezadoCompra.idEncCompraProductos = encabezadoCompraJSON["ID_ENC_COMPRA_PRODUCTOS"] as! Int
+                            
+                        }
+                        
+                        completion(encabezadoCompra.idEncCompraProductos)
+                        return
+                    }
+                } else {
+                    completion(-2)
+                    return
+                }
+            }catch let parseError {//manejamos el error
+                
+                print("Error al parsear: \(parseError)")
+                
+                let responseString = String(data: data, encoding: .utf8)
+                
+                print("respuesta: \(responseString!)")
+                
+                completion(-2)
+                return
+            }
+        }
+        
+        task.resume()
+    }
+    
+    //Metodo para insertar detalle compra
+    func insertarDetalleCompraPoductos(idEncabezado:Int, idProducto:Int, cantidad:Int, precioLote:Double, precioUnitario:Double, precioUnitarioReal:Double, porcentajeGanancia:Double, completion:@escaping(_ r:Int) -> ()){
         
         print("Enviar solicitud")
         
@@ -141,12 +285,13 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
         request.httpMethod = "POST"
         
         let parameters: [String: Any] = [
-            "idCategoria": idCategoria,
-            "idUsuario": idUsuario,
-            "nombreProducto" : nombreProducto,
-            "precio" : precio,
-            "existencia" : existencia,
-            "detalles" : detalles
+            "idEncabezado": idEncabezado,
+            "idProducto": idProducto,
+            "cantidad" : cantidad,
+            "precioLote" : precioLote,
+            "precioUnitario" : precioUnitario,
+            "precioUnitarioReal" : precioUnitarioReal,
+            "porcentajeGanancia" : porcentajeGanancia
         ]
         
         request.httpBody = parameters.percentEncoded()
@@ -154,7 +299,7 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
             guard let data = data else { //si existe un error se termina la funcion
                 print("solicitud fallida \(String(describing: error))") //manejamos el error
                 
-                completion(-3)
+                completion(-2)
                 return
             }
             
@@ -177,7 +322,7 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
                         return
                     }
                 } else {
-                    completion(-3)
+                    completion(-2)
                     return
                 }
             }catch let parseError {//manejamos el error
@@ -188,7 +333,7 @@ class CompraProductos: UIViewController, UITextFieldDelegate {
                 
                 print("respuesta: \(responseString!)")
                 
-                completion(-3)
+                completion(-2)
                 return
             }
         }
